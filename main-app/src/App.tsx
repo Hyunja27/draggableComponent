b7e7c64f17b8825b3db65581ee8578069abb51ea
hyunja27 <123gk@naver.com>
2022-05-24 18:27:26 +0900

    :building_construction: 05/24_delete_overlapped_main_app_folder #3

--- main-app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Draggable from "./component/Draggable";
-
-export default function App() {
-  return (
-    <Draggable/>
-  );
-}
--- main-app/src/component/Draggable.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Draggable(){
-    return(
-        <div>
-            This is basic box Component.
-        </div>
-    );
-}
